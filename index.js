acccde6880ac4cf4827c9b666adf4cce96c9ff09
Franco <franco.danielsen@gmail.com>
2022-07-16 22:07:43 -0300

    commiting frontend first views

---
+++ index.js
@@ -0,0 +1,6 @@
+iconMenu.onclick = () => {
+    menu.classList.toggle("show")
+}
+unorderList.onclick = () => {
+    menu.classList.remove("show")
+}
---
+++ scripts/elements.js
@@ -0,0 +1,3 @@
+let iconMenu = document.querySelector("#icon");
+let menu = document.querySelector(".menu");
+let unorderList = document.querySelector(".menu__list");
